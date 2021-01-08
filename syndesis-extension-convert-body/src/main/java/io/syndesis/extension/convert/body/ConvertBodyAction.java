2010ccfbb840aa317769bc70f607600ea56dc36e
Anand Kane <anand@infracloud.io>
2021-01-08 12:04:57 +0530

    moved extension definition from java to json

--- syndesis-extension-convert-body/src/main/java/io/syndesis/extension/convert/body/ConvertBodyAction.java
+++ syndesis-extension-convert-body/src/main/java/io/syndesis/extension/convert/body/ConvertBodyAction.java
@@ -1,23 +1,42 @@
 package io.syndesis.extension.convert.body;
 
-import java.util.Map;
-import java.util.Optional;
-
+import io.syndesis.extension.api.Step;
+import io.syndesis.extension.api.annotations.Action;
 import org.apache.camel.CamelContext;
 import org.apache.camel.model.ProcessorDefinition;
 import org.apache.camel.util.ObjectHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import io.syndesis.extension.api.Step;
-import io.syndesis.extension.api.annotations.Action;
+import java.util.Map;
+import java.util.Optional;
 
-@Action(id = "ConvertBody", name = "Convert Body", description = "Convert Body to a plain String", tags = { "body", "extension" })
+//@Action(id = "ConvertBody", name = "Convert Body", description = "Convert Body to a plain String", tags = {"body", "extension"})
 public class ConvertBodyAction implements Step {
+    private static Logger logger = LoggerFactory.getLogger(ConvertBodyAction.class);
 
-	@Override
+    @Override
     public Optional<ProcessorDefinition<?>> configure(CamelContext context, ProcessorDefinition<?> route, Map<String, Object> parameters) {
-		ObjectHelper.notNull(route, "route");
+        ObjectHelper.notNull(route, "route");
 
-		route.convertBodyTo(String.class);
-		return Optional.empty();
-	}
+//        ProcessorDefinition<?> processor = route.process(exchange -> {
+//            String body = exchange.getIn().getBody(String.class);
+//            route.setBody(() -> "Additional text" + body);
+//        });
+//        return Optional.of(processor);
+
+        route.setBody(exchange -> {
+            logger.info("Setting body inside lambda");
+            return "Additional text " + exchange.getIn().getBody(String.class);
+        });
+
+        route.convertBodyTo(String.class);
+
+//        route.setBody(() -> "Addition text");
+        System.out.println("Keys in parameter map :" + parameters.keySet());
+        logger.info("Keys in parameter map :" + parameters.keySet());
+        System.out.println("Parameter map: " + parameters);
+        logger.info("Parameter map: " + parameters);
+        return Optional.empty();
+    }
 }
